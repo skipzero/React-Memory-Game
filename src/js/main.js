9a67ee8fe6e0e8bc63424d4d80c67926cb42e8bd
Skip McBride <skip@zerosquadron.com>
2016-09-19 11:47:52 -0700

    cards flipp correctly now

--- src/js/main.js
+++ src/js/main.js
@@ -11,23 +11,92 @@ const ABRA = 'http://cdn.bulbagarden.net/upload/6/62/063Abra.png'
 const GYARADOS = 'http://cdn.bulbagarden.net/upload/4/41/130Gyarados.png'
 const MEWTWO = 'http://cdn.bulbagarden.net/upload/thumb/7/78/150Mewtwo.png/250px-150Mewtwo.png'
 
-const pokeArray = ('BULBASAUR CHARMANDER SQUIRTLE PIKACHU JIGGLYPUFF ABRA GYARADOS MEWTWO').toLowerCase().split(' ');
+//  our monsterous array!
+let pokeArray = 'BULBASAUR, CHARMANDER, SQUIRTLE, PIKACHU, JIGGLYPUFF, ABRA, GYARADOS, MEWTWO';
+const arrCopy = pokeArray.slice();
+
+pokeArray = pokeArray.toLowerCase().split(', ');
+
+console.log('COPY', arrCopy);
 
 class Concentration extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
+		this.restart = this.restart.bind(this);
+		this.timer = null;
+
+		this.checkMatch = this.checkMatch.bind(this);
+
+		this.state = this.cleanState();
+
+		this.state.deck = this.shuffleDeck();
+	}
+
+	cleanState() {
+		return {
 			deck: [],
-			matches: 0,
+			matches: [],
 			moves: 0,
+			selected: [],
 		};
+	}
+
+	gameBoard() {
+		return (
+			<div id='gameBoard'> {
+					this.state.deck.map((card, i) => {
+						return (
+							<Card
+								className={card}
+								handleClick={this.clickHandler.bind(this, i)}
+								index={i}
+								id={i}
+								isSelected={this.state.selected.includes(i)}
+								key={i}
+								didMatch={this.state.matches.includes(i)}
+							/>
+						);
+					}, this)
+				}
+			</div>
+		)
+	}
+
+	clickHandler(cardId) {
+		if(this.restartTimer || this.state.selected.includes(cardId)) {
+			return;
+		}
+
+		if(this.state.selected.length >= 1) {
+			this.restartTimer = setTimeout(this.checkMatch(), 1000);
+		}
+    console.log(cardId, 'PROPS', this);
+		this.setState({ selected: this.state.selected.concat(cardId)});
+	}
+
+	checkMatch() {
+		console.log('Checking the matches...')
+		let matches = this.state.matches.slice();
+		const selected = this.state.selected.map((id) => this.state.deck[id]);
+
+		if(pokeArray[selected[0]] === pokeArray[selected[1]]) {
+			matches = matches.concat(this.state.selected);
+						console.log('Selections', selected[0], selected[1]);
+		}
+console.log('Matcher', matches)
+		this.setState({
+			moves: this.state.moves += 1,
+			selected: [],
+			matches: matches,
+		});
+		this.timer = null;
 	}
 
 	render() {
 		const gameboard = this.gameBoard();
-
   	return (
 			<div>
+				<div className='score'><span> </span></div>
 				{gameboard}
 			</div>
 		);
@@ -53,8 +122,9 @@ class Concentration extends React.Component {
 		return deck;
   }
 
-	//  Shuffle our cards after they're picked...
-  makeDeck() {
+	//  Shuffle our cards after they're picked using the Fisher-Yates shuffle:
+  // https://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+  shuffleDeck() {
     let deck = this.pickCards();
     for(let i = deck.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -62,51 +132,42 @@ class Concentration extends React.Component {
       deck[i] = deck[j];
       deck[j] = tempVal;
     }
+		console.log('Shuffled...', deck)
     return deck;
   }
 
-	clickHandler() {
-    console.log('PROPS', this.state);
-    this.state.moves += 1;  //  increament our moves
-		this.pickCards();
-	}
-
 	randomNumber() {
 		const min = 0;
-		const max = pokeArray.length - 1;
-
+		const max = pokeArray.length - 1;  //  using length of our array so we never get a number out of range
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 
-	gameBoard() {
-		console.log(this.makeDeck());
-		return (
-			<div id='gameBoard'> {
-					this.state.deck.map((card, i) => {
-						return (
-							<Card index={i} key={i} className={card} clickMon={this.clickHandler.bind(this)} />
-						);
-					}, this)
-				}</div>
-		)
+	//  reset our game when all cards are matched...
+	restart() {
+		this.setState(this.cleanState());
 	}
 };
 
 //  Define our card class...
+//  Normally we'd separate this into it's own file
 class Card extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
-		return (
-			<div className={`card back ${this.props.className}`} onClick={this.handleClick.bind(this)}> </div>
-		);
-	}
+		const classes = this.props.className;
+		const turned = this.props.isSelected ? 'card flipped' : 'card';
+		const toggleVisible = this.props.didMatch ? 'hidden' : 'visible';
 
-	handleClick() {
-    this.props.clickMon(this.props.pokemon, this.props.className)
-    console.log('Card Props', this.props)
+		let style = {
+			visibility: toggleVisible,
+		};
+
+		return (
+			<div className='flip' id={this.props.id} onClick={this.props.handleClick.bind(this)}>
+				<div className={turned} style={style}>
+					<div className={`face front ${this.props.className}`}> </div>
+					<div className='face back'> </div>
+				</div>
+			</div>
+		);
 	}
 }
 
